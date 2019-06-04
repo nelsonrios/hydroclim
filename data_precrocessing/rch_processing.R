c7041d7515ed81f8f58154740721b5fa944c812e
Xiaojun Wang <48254997+susan1637@users.noreply.github.com>
2019-06-04 09:16:41 -0500

    rcp 85

--- data_precrocessing/rch_processing.R
+++ data_precrocessing/rch_processing.R
@@ -88,12 +88,14 @@ rcpprocessrch <- function(basin_id){
   shape <- readOGR(paste(root_shapefiles,basin_name,"/GIS_data/Reach/Reach.shp"))
   len <- length(shape)
   
-  #RCP 4.5
+  
   model_info_dat <- read.csv("model_info.csv", sep = ",", header = T )
   model_info_dat <- data.frame(model_info_dat)
-  model_ids <- model_info_dat$model_id
+  model_45ids <- model_info_dat[which(model_info_dat$model_id == 1)]
+  model_85ids <- model_info_dat[which(model_info_dat$model_id == 2)]
   
-  for(model_id in model_ids){
+  #RCP 4.5
+  for(model_id in model_45ids){
     model_name <- trimws(model_info_dat[model_id,2])
 
     dat <- read.csv(paste(root_shapefiles,basin_name,"/RCP_4.5/",model_name,"/",model_name,".shp"), sep = "", skip = 9, header = F )
@@ -134,10 +136,55 @@ rcpprocessrch <- function(basin_id){
       currentYear <- currentYear+1;
       monthCount <- 1;
   }
-
+    write.csv(monthdat, file=paste(basin_name,"_RCP_4.5_",model_name,"_monthlyData.csv"))
   }
-  write.csv(monthdat, file=paste(basin_name,"RCP_4.5_monthlyData.csv"))
-  return(monthdat)
+  
+  #RCP 8.5
+  for(model_id in model_85ids){
+    model_name <- trimws(model_info_dat[model_id,2])
+    
+    dat <- read.csv(paste(root_shapefiles,basin_name,"/RCP_8.5/",model_name,"/",model_name,".shp"), sep = "", skip = 9, header = F )
+    rchData <- dat[c(2, 4, 5, 7, 51)]
+    colnames(rchData) <- c("RCH", "MON", "AREAkm2", "FLOW_OUTcms", "WTMPdegc")
+    monthdat <- subset(rchData, MON <= 12)
+    
+    joined <- merge(shape@data, monthdat, by.x="OBJECTID", by.y="RCH")
+    shape@data <- joined
+    
+    
+    totalCount <- 1
+    
+    pointCount <- 1
+    totalPoints <- len #totally numners of rows in reach shapefiles
+    
+    yearCount <- 1
+    totalYears <- 150
+    
+    monthCount <- 1
+    currentYear <- 1950
+    
+    yearMonthCount <- 1
+    
+    while(yearCount <= totalYears){
+      while(monthCount <= 12) {
+        while (pointCount <= totalPoints) {
+          monthdat[totalCount,"YEAR"] <- currentYear;
+          monthdat[totalCount,"YearMonthId"] <- yearMonthCount;
+          pointCount <- pointCount+1;
+          totalCount <- totalCount+1;
+        }
+        pointCount <- 1;
+        monthCount <- monthCount+1;
+        yearMonthCount <- yearMonthCount +1;
+      }
+      yearCount <- yearCount+1;
+      currentYear <- currentYear+1;
+      monthCount <- 1;
+    }
+    write.csv(monthdat, file=paste(basin_name,"_RCP_8.5_",model_name,"_monthlyData.csv"))
+  }
+  
+  #return(monthdat)
 }
   
 system.time({
@@ -155,16 +202,16 @@ system.time({
 
   
 ##WRITE MONTHLY DATA TO POSTGRES
-  monthdat <- read.csv(paste(root_shapefiles,basin_name,"observerd_monthlyData.csv"))
-  monthdat <- monthdat[c(2, 4, 5, 6, 8)]
-  colnames(monthdat)[5] <- "record_month_year_id"
-  names(monthdat) <- tolower(names(monthdat))
-  monthdat$Id <- seq.int(nrow(monthdat))
-  monthdat$If_observed <- FALSE; # add observed flag
-  monthdat$model_id <- NA; # set model is as NA
-  res <- dbWriteTable(con, "reach_data", 
-               value = monthdat, append = TRUE, row.names = FALSE)
-  print(res)
+#  monthdat <- read.csv(paste(root_shapefiles,basin_name,"observerd_monthlyData.csv"))
+#  monthdat <- monthdat[c(2, 4, 5, 6, 8)]
+#  colnames(monthdat)[5] <- "record_month_year_id"
+#  names(monthdat) <- tolower(names(monthdat))
+#  monthdat$Id <- seq.int(nrow(monthdat))
+#  monthdat$If_observed <- FALSE; # add observed flag
+#  monthdat$model_id <- NA; # set model is as NA
+#  res <- dbWriteTable(con, "reach_data", 
+#               value = monthdat, append = TRUE, row.names = FALSE)
+#  print(res)
 
 
 
